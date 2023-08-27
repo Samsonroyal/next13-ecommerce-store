759705d09c5e4dbbc6b9497d11d9fe873287bcd8
Were Samson Bruno <werefast2000@gmail.com>
2023-08-27 18:53:53 +0300

    custom footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,15 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2023 Store, Inc. All rights reserved.
+          &copy; 2023 <a href="https://taasa.app" target="_blank" rel="noopener noreferrer">Taasa Marketplace,</a> all rights reserved. Built with {" "}
+          <span role="img" aria-label="love">
+            ❤️
+          </span>{" "}
+          by the people at <a href="https://taasa.app" target="_blank" rel="noopener noreferrer">Taasa</a>.
         </p>
       </div>
     </footer>
-  )
+  );
 };
 
 export default Footer;
